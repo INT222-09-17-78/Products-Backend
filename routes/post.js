b03c2758f2df34d2238b0c6feee686507ce97e61
Kampol Thaipricha <kampol.43@mail.kmutt.ac.th>
2021-08-06 12:28:08 +0700

    add postController

--- routes/post.js
+++ routes/post.js
@@ -2,44 +2,17 @@ const express = require('express')
 
 const router = express.Router()
 
-const Post = require('../models/PostModel')
+
 // router.get('/posts', (req,res) => {
 //     res.send('on post')
 // })
+const postController = require('../controllers/postController') 
 
-router.get('/', async (req,res) => {
-    try {
-        const getAllPost = await Post.find()
-        res.json(getAllPost)
-    } catch (error) {
-        res.json(error)
-    }
-})
+router.get('/', postController.getAllPost)
 
-router.post('/', async (req,res) => {
-    const post = new Post({
-        test: req.body.test
-    })
-    try {
-        const savePost = await post.save();
-        res.json(savePost)
-    } catch (error) {
-        res.json(error)
-    }
-        
-    
-    console.log(Number.isInteger(req.body.test));
-})
+router.post('/', postController.postOne)
 
-router.get('/:postId', async (req,res) => {
-    try{
-    const post = await Post.findById(req.params.postId)
-    res.json(post)
-    console.log(req.params.postId)
-}catch(error){
-    res.json(error)
-}
-})
+router.get('/:postId', postController.getById)
 
 // router.delete('/:Id', async (req,res) => {
     
@@ -53,35 +26,8 @@ router.get('/:postId', async (req,res) => {
 //     // }
 // })
 
-router.delete('/:postId', async (req,res) => {
-    try{
-    const post = await Post.findByIdAndDelete(req.params.postId)
-    res.json(post)
-    console.log(req.params.postId)
-}catch(error){
-    res.json(error)
-}
-})
+router.delete('/:postId', postController.deleteById)
 
-router.put('/:postId', async (req,res) => {
-    try{
-    // const post = await Post.findByIdAndUpdate({_id : req.params.postId} , req.body).then(() => Post.findOne({_id : req.params.postId}))
-    const post = await Post.updateOne({_id : req.params.postId} , {$set: {test: req.body.test}})
-    res.json(post)
-    console.log(req.params.postId)
-}catch(error){
-    res.json(error)
-}
-
-// const post = new Post({
-//     test: req.body.test
-// })
-// try {
-//     const savePost = await post.save();
-//     res.json(savePost)
-// } catch (error) {
-//     res.json(error)
-// }
-})
+router.put('/:postId', postController.updateById)
 
 module.exports = router;
