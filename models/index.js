512c9cf7f8dec0fa1b9d9459d898294ce495c18a
Kampol Thaipricha <kampol.43@mail.kmutt.ac.th>
2021-09-15 18:41:49 +0700

    add dockerfile

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const {Sequelize ,DataTypes } = require('sequelize')
 const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
     host: process.env.HOST,
-    dialect: process.env.dialect,
+    dialect: process.env.DIALECT,
     operatorsAliases: 0,
   
     pool: {
