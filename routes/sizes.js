f0f63610afd6e526ce312a960b7cb438c4c8eef1
Kampol-Nui <kampol.43@mail.kmutt.ac.th>
2021-11-25 01:46:42 +0700

    update  workflow

--- routes/sizes.js
+++ routes/sizes.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const sizesController = require('../controllers/sizesController');
 
 router.post('/create/size', sizesController.createSize)
-router.get('/show/sizesssssss' , sizesController.getAllSizes)
+router.get('/show/sizes' , sizesController.getAllSizes)
 module.exports = router;
