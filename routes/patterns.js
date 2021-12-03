49e22e625b58af884ee97a07711a054664bf7738
Kampol-Nui <kampol.43@mail.kmutt.ac.th>
2021-12-03 23:23:16 +0700

    update delete pattern

--- routes/patterns.js
+++ routes/patterns.js
@@ -6,5 +6,5 @@ const upload = require('../controllers/uploadsController')
 router.post('/create/pattern', upload.uploadFileArray,pattern.createPattern)
 router.get('/show/patterns' , pattern.getAllPatterns)
 router.put('/update/patterns' , upload.uploadFileArray , pattern.editPattern)
-router.delete('/delete/:PatternName' , pattern.deletePatterns)
+router.delete('/delete/pattern/:PatternName' , pattern.deletePatterns)
 module.exports = router;
