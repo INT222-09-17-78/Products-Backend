b9ed50c6642df3a14877e1979196a503c8f938c7
Kampol-Nui <kampol.43@mail.kmutt.ac.th>
2021-12-05 00:10:11 +0700

    update image

--- routes/upload.js
+++ routes/upload.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 const upload = require('../controllers/uploadsController')
 router.get('/show/images' ,upload.getFilesList)
-router.get('/download/image/:name' , upload.downloadPatternsFiles)
+router.get('/download/image/pattern/:name' , upload.downloadPatternsFiles)
+router.get('/download/image/:name' , upload.downloadFiles)
 module.exports = router;
