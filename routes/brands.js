fa1578fc95efab226c44d8358841c3333525b6c0
Kampol-Nui <kampol.43@mail.kmutt.ac.th>
2021-12-04 15:44:04 +0700

    test authen

--- routes/brands.js
+++ routes/brands.js
@@ -4,7 +4,7 @@ const auth = require('../middleware/auth')
 const router = express.Router()
 const brandsController = require('../controllers/brandsController')
 router.post('/create/brand',brandsController.createBrand)
-router.get('/show/brands',auth.validateTokenPublic, brandsController.getAllBrands)
+router.get('/show/brands', brandsController.getAllBrands)
 router.put('/update/brand' , brandsController.editBrand)
 router.delete('/delete/brand/:BrandId' , brandsController.deleteBrand)
 module.exports = router;
