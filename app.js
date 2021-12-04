178637c9aca25e60e2f99fd24e66e574fce67f7a
Kampol-Nui <kampol.43@mail.kmutt.ac.th>
2021-12-04 14:03:28 +0700

    test authen

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use('/api' , usersRoutes , productsRoutes, brandsRoutes , uploadsRoutes , Si
 //     res.json({ message: "Welcome to bezkoder application." });
 //   });
 
-db.sequelize.sync({force:true}).then((res) => {
+db.sequelize.sync().then((res) => {
     app.listen(process.env.PORT, () => {
         console.log('server is running on port '+process.env.PORT)
     })
